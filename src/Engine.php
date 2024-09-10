b7437e08430c4b9cf42ce5132acbcb94152784ea
octodes <octode@gmail.com>
2024-09-10 22:32:02 +0300

    Refactor for proper engine-runner-logic model

--- src/Engine.php
+++ src/Engine.php
@@ -2,21 +2,54 @@
 
 namespace BrainGames\Engine;
 
-use function BrainGames\Greeting\greetGetName;
+use function BrainGames\Even\GetEvenRules;
+use function BrainGames\Even\getEvenNumber;
 use function cli\line;
 use function cli\prompt;
 
-function playBrainGame(string $gameType, string $message) {
-    $name = greetGetName();
+function playBrainGame(string $gameType) {
     $correctAnswersCount = 0;
     $winRoundsCount = 3;
 
-    line($message);
+    $name = greetGetName();
+
+    line(showRules($gameType));
     
     while ($correctAnswersCount < $winRoundsCount) {
-        [$question, $correctAnswer] = 
+        [$question, $correctAnswer] = setGame($gameType);
+        line("Question: $question");
+        $answer = prompt("Your answer");
+
+        if ($answer !== $correctAnswer) {
+            line("'$answer' is wrong answer ;(. Correct answer was '$correctAnswer'.");
+            line("Let's try again, $name!");
+            return false;
+        }
+
+        line("Correct!");
+        $correctAnswersCount++;
     }
 
-    
+    line("Congratulations, $name!");
+}
 
+function greetGetName(): string
+{
+    line('Welcome to the Brain Game!');
+    $name = ucfirst(prompt('May I have your name?'));
+    line("Hello, $name!");
+
+    return $name;
+}
+
+function setGame(string $gameType): array {
+    return match ($gameType) {
+        'even' => getEvenNumber()
+    };
+}
+
+function showRules(string $gameType): string {
+    return match ($gameType) {
+        'even' => GetEvenRules()
+    };
 }
---
+++ src/Games/Even.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace BrainGames\Even;
+
+function getEvenRules(): string {
+    $message = 'Answer "yes" if the number is even, otherwise answer "no".';
+    return $message;
+}
+    
+function getEvenNumber()
+{
+    $question = rand(1, 99);
+    $question % 2 === 0 ? $correctAnswer = 'yes' : $correctAnswer = 'no'; 
+    return [$question, $correctAnswer];
+}
